251343226b1843c5cefed045b092b59b1c693c65
imyofather79 <chunsum708@gmail.com>
2021-05-26 00:01:54 -0600

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+
+
+function upperCaseCustomerName() {
+    name = customerName.toUpperCase();
+    customerName = name;
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() { 
+    let newbestCustomer = 'maybe bob';
+    bestCustomer = newbestCustomer;
+}
+
+const leastFavoriteCustomer = 'tom';
+
+function changeLeastFavoriteCustomer() {
+    let newLeastFavoriteCustomer = 'Peter';
+    leastFavoriteCustomer = newLeastFavoriteCustomer;
+}
